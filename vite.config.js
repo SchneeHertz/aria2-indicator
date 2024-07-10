64471015762b6998d51afd1b59b2c83ea2695ff7
SchneeHertz <39257008+SchneeHertz@users.noreply.github.com>
2024-07-10 10:09:59 +0800

    build after HMR

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, build } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import fs from 'fs/promises'
+
+let bundling = false
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    {
+      name: 'hot-build',
+      enforce: "pre",
+      async handleHotUpdate() {
+        if (!bundling) {
+          bundling = true
+          await build({'build': { outDir: './dist'}})
+          bundling = false
+        }
+        return []
+      }
+    },
     {
       name: 'copy-file',
       apply: 'build',
