43444c26764bb55b4321069b0ea63ca500ecad13
SchneeHertz <39257008+SchneeHertz@users.noreply.github.com>
2024-07-10 11:30:20 +0800

    update readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# Vue 3 + Vite
+<div align="center">
 
-This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+<img src="https://raw.githubusercontent.com/SchneeHertz/aria2-indicator/master/crx_file/icon/Ar_128.png" alt="icon.png" width="128"/>
 
-Learn more about IDE Support for Vue in the [Vue Docs Scaling up Guide](https://vuejs.org/guide/scaling-up/tooling.html#ide-support).
+# Aria2 Indicator
+
+</div>
--- src/components/Task.vue
+++ src/components/Task.vue
@@ -8,7 +8,9 @@
         <n-gi :span="4" class="task-item">
           <n-flex justify="center">
             <n-button text class="task-button" type="error" @click="removeTask">
-              <n-icon><Delete /></n-icon>
+              <n-icon>
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32"><path d="M12 12h2v12h-2z" fill="currentColor"></path><path d="M18 12h2v12h-2z" fill="currentColor"></path><path d="M4 6v2h2v20a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8h2V6zm4 22V8h16v20z" fill="currentColor"></path><path d="M12 2h8v2h-8z" fill="currentColor"></path></svg>
+              </n-icon>
             </n-button>
           </n-flex>
         </n-gi>
@@ -40,7 +42,6 @@
 <script setup>
 import { computed } from 'vue'
 import { NCard, NGrid, NGi, NText, NButton, NIcon, NFlex, NProgress } from 'naive-ui'
-import { Delete } from '@vicons/carbon'
 
 const props = defineProps({
   task: {
